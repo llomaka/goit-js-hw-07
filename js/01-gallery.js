f355dface6132da7808dda5911b14b9244c9f795
Lilia Lomaka <llomaka80@gmail.com>
2022-02-03 22:34:09 +0200

    Створення розмітки завдання 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,4 +2,30 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const listRef = document.querySelector('.gallery');
 
+function renderArrayElements(array) {
+    return array.map(({ description, original, preview }) => {
+        return `<div class="gallery__item">
+    <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</div>`;
+    }).join('');
+}
+
+const markup = renderArrayElements(galleryItems);
+listRef.insertAdjacentHTML('beforeend', markup);
+listRef.addEventListener('click', handleImageClick);
+
+function handleImageClick(event) { 
+    event.preventDefault();
+    console.dir(event.target);
+}
+
+
+
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,4 +2,6 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const listRef = document.querySelector('.gallery');
 
+
+
 console.log(galleryItems);
