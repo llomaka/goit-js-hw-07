06812b8fd079fef1c272ee4224f73e4bc26c863c
Lilia Lomaka <llomaka80@gmail.com>
2022-02-04 12:42:49 +0200

    Завдання 2 виконано

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -5,27 +5,16 @@ const listRef = document.querySelector('.gallery');
 function renderArrayElements(array) {
     return array.map(({ description, original, preview }) => {
         return  `<li><a class="gallery__item" href=${original}>
-        <img class="gallery__image" src=${preview} alt=${description} />
+        <img class="gallery__image" src=${preview} alt="${description}" />
         </a></li>`;
     }).join('');
 }
 
 const markup = renderArrayElements(galleryItems);
 listRef.insertAdjacentHTML('beforeend', markup);
-let lightbox = new SimpleLightbox('.gallery a');
+let lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
 listRef.addEventListener('click', handleImageClick);
 
 function handleImageClick(event) { 
     event.preventDefault();
-    
-    // console.log(lightbox);
-    // lightbox.on('show.simplelightbox', function (evt) {
-    //     console.log(evt);
-    // });
-    // gallery.on('error.simplelightbox', function (e) {
-    //     console.log(e);
-    // });
 }
-
-
-
