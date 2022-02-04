7223fdbd531adffe1785bc509bfd2cacd524e992
Lilia Lomaka <llomaka80@gmail.com>
2022-02-04 11:52:39 +0200

    Налантування бібліотеки завдання 2, скидання базових стилів css для img

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,6 +2,30 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const listRef = document.querySelector('.gallery');
 
+function renderArrayElements(array) {
+    return array.map(({ description, original, preview }) => {
+        return  `<li><a class="gallery__item" href=${original}>
+        <img class="gallery__image" src=${preview} alt=${description} />
+        </a></li>`;
+    }).join('');
+}
+
+const markup = renderArrayElements(galleryItems);
+listRef.insertAdjacentHTML('beforeend', markup);
+let lightbox = new SimpleLightbox('.gallery a');
+listRef.addEventListener('click', handleImageClick);
+
+function handleImageClick(event) { 
+    event.preventDefault();
+    
+    // console.log(lightbox);
+    // lightbox.on('show.simplelightbox', function (evt) {
+    //     console.log(evt);
+    // });
+    // gallery.on('error.simplelightbox', function (e) {
+    //     console.log(e);
+    // });
+}
 
 
-console.log(galleryItems);
+
